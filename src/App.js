c3e53f769c795ed4a4f059ce79dce9c1015fef03
Kenec <nnamani.kenechukwu@gmail.com>
2018-11-16 15:25:41 +0100

    click a single card

--- src/App.js
+++ src/App.js
@@ -5,27 +5,30 @@ import Card from './components/card/Card';
 import './styles/main.css';
 
 class App extends Component {
+
+  state = { 
+    isFlipped: Array(16).fill(false)
+  }
+
+  handleClick = event => {
+    event.preventDefault();
+    const cardId = event.target.id;
+    const newFlipps = this.state.isFlipped.slice();
+    newFlipps[cardId] = !newFlipps[cardId];
+
+    this.setState(prevState => ({ 
+      isFlipped: newFlipps
+    }));
+  }
+
   render() {
     return (
      <div>
        <Header />
        <div className="grid-container">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card id={0} isFlipped={this.state.isFlipped[0]} handleClick={this.handleClick} />
+          <Card id={1} isFlipped={this.state.isFlipped[1]} handleClick={this.handleClick} />
+          
         </div>
      </div>
     );
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ReactCardFlip from "react-card-flip";
 
-const Card = () => (
-  <button className="card">
-    Front
-  </button>
+const Card = ({ id, isFlipped, handleClick }) => (
+  <ReactCardFlip isFlipped={isFlipped}>
+    <button id={id} className="card card-front" onClick={handleClick} key="front">
+    </button>
+
+    <button id={id} className="card card-back" onClick={handleClick} key="back">
+    </button>
+  </ReactCardFlip>
 );
 
 export default Card;
