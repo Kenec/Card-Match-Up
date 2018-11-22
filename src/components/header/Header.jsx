00fbd46bf3c47c33e808a6c7b91e634a457a228b
Kenec <nnamani.kenechukwu@gmail.com>
2018-11-22 20:56:06 +0100

    remove timer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ restartGame }) => (
   <div className="grid-header-container">
-    <div className="justify-left timer">Timer: 0</div>
+    <div className="justify-left timer"></div>
     <div className="justify-center game-status-text"></div>
     <div className="justify-end">
       <button onClick={restartGame} className="restart-button">Restart Game</button>
